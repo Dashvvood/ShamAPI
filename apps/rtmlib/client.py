e70e80800b683a273fe28ad211b4bba0ce76507a
Dashvvood <780966523@qq.com>
2025-09-25 20:20:47 +0800

    :art: ADD rtmlib

---
+++ apps/rtmlib/client.py
@@ -0,0 +1,25 @@
+
+from urllib import response
+import requests
+import uuid
+from rtmlib import draw_skeleton
+import cv2
+import numpy as np
+
+def client(filepath, url="http://127.0.0.1:8000/predict"):
+    files = {"file":  open(filepath, "rb")}
+    data = {"RequestID": uuid.uuid4().hex}
+    response = requests.post(url, data=data, files=files)
+    result = response.json()
+    img = cv2.imread(filepath)
+    keypoints = np.array(result['ModelOutput']['keypoints'])
+    scores = np.array(result['ModelOutput']['scores'])
+    draw_skeleton(img, keypoints, scores)
+    return result, img
+
+if __name__ == "__main__":
+    result, img = client("./data/example01.jpg")
+    print(result)
+    cv2.imshow("Result", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
---
+++ apps/rtmlib/main.py
@@ -0,0 +1,44 @@
+import os
+import elinor
+# 环境变量
+DOTENV = elinor.fast_loadenv_then_append_path()
+from fastapi import FastAPI, UploadFile, File, Form
+import cv2
+import numpy as np
+from rtmlib import Wholebody
+from omegaconf import OmegaConf
+from loguru import logger
+import uvicorn
+
+o_d = elinor.o_d()
+config = OmegaConf.load("config/default.yaml")
+logger.add(**config["log"])
+model = Wholebody(**config["model"])
+app = FastAPI(title="HPE", version=0.1)
+
+@app.get("/")
+async def index():
+    return config
+
+@app.post("/predict")
+async def predict(RequestID: str = Form(...), file: UploadFile = File(...)):
+    logger.info(f"RequestID: {RequestID}, filename: {file.filename}, content_type: {file.content_type}")
+    contents = await file.read()
+    img = cv2.imdecode(np.frombuffer(contents, np.uint8), cv2.IMREAD_COLOR)
+    keypoints, scores = model(img)
+    return {
+        "RequestID": RequestID,
+        "ModelOutput": {
+            "keypoints": keypoints.tolist(),
+            "scores": scores.tolist()
+        }
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app, 
+        host=config["app"]["host"], 
+        port=config["app"]["port"],
+        # workers=config["app"]["workers"],
+        # reload=config["app"]["reload"]
+    )
