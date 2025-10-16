8021ea68247ed35849ae1722f8f16bf47d5b8114
Dashvvood <780966523@qq.com>
2025-10-16 17:56:26 +0800

    :art: for RTM-HPE

--- apps/RTM-HPE/client.py
+++ apps/RTM-HPE/client.py
@@ -1,37 +1,3 @@
-# import requests
-# # import uuid
-# from rtmlib import draw_skeleton
-# import cv2
-# import numpy as np
-# import asyncio  
-
-
-# async def client(filepath, url="http://127.0.0.1:8000/predict"):
-#     files = {"file":  open(filepath, "rb")}
-#     data = {"RequestID": uuid.uuid4().hex}
-#     response = requests.post(url, data=data, files=files)
-#     print(response)
-#     result = response.json()
-#     img = cv2.imread(filepath)
-#     keypoints = np.array(result['ModelOutput']['keypoints'])
-#     scores = np.array(result['ModelOutput']['scores'])
-#     draw_skeleton(img, keypoints, scores)
-#     cv2.imwrite(f"processed_{uuid.uuid4().hex}.jpg", img)
-#     print(f"ProcessedTime: {result['ProcessedTime']}")
-#     return result
-
-# async def main():
-#     imgs = ["./data/example01.jpg", "./data/example02.jpg"] * 10
-#     tasks = [client(img) for img in imgs]
-    
-#     for completed_task in asyncio.as_completed(tasks):
-#         result = await completed_task
-
-#     cv2.destroyAllWindows()
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
-
 import requests
 from PIL import Image
 from elinor.cv import pil_to_b64
@@ -45,4 +11,8 @@ message = {
 url = "http://127.0.0.1:8000/predict"
 
 response = requests.post(url, json=message)
-print(response.json())
+print(f"{response.status_code = }")
+data = response.json()
+print(data["model_output"].keys())
+
+
